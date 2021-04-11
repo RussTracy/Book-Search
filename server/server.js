b1a487a4930150c6c655d9366952afd141501122
Russ <ocjade@comcast.net>
2021-04-10 19:36:53 -0600

    Apollo Server Working

--- server/server.js
+++ server/server.js
@@ -3,8 +3,23 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
+// import Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// integrate our Apollo Server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
